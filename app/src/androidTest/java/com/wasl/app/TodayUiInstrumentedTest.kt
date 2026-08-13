06dc8b6fea88e99a05d296e7e0137d16c4d87591
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-13 10:39:58 -0700

    test(app): align Today assertion with Compose API

--- app/src/androidTest/java/com/wasl/app/TodayUiInstrumentedTest.kt
+++ app/src/androidTest/java/com/wasl/app/TodayUiInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package com.wasl.app
 
 import android.content.Context
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag

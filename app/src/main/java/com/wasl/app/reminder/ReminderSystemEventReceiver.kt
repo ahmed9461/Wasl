c40a652c7b5792cef02958d367debf2a34f8a38c
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-25 15:45:15 -0700

    Recover general reminders after system changes

--- app/src/main/java/com/wasl/app/reminder/ReminderSystemEventReceiver.kt
+++ app/src/main/java/com/wasl/app/reminder/ReminderSystemEventReceiver.kt
@@ -11,8 +11,13 @@ class ReminderSystemEventReceiver : BroadcastReceiver() {
             Intent.ACTION_TIME_CHANGED,
             Intent.ACTION_TIMEZONE_CHANGED,
             Intent.ACTION_BOOT_COMPLETED,
-            AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED,
-            -> WorkManagerReminderScheduler(context).requestRecovery()
+            -> {
+                WorkManagerReminderScheduler(context).requestRecovery()
+                WorkManagerGeneralReminderScheduler(context).requestRecovery()
+            }
+
+            AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED ->
+                WorkManagerReminderScheduler(context).requestRecovery()
         }
     }
 }

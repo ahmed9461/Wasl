8e49f4561241b26ca6c17be760b2239d3e71995d
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-27 11:15:42 -0700

    fix: import layout weight for statistics rows

--- app/src/main/java/com/wasl/app/StatisticsScreen.kt
+++ app/src/main/java/com/wasl/app/StatisticsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card

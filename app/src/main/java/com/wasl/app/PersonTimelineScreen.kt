8dc8f60e78b8bc39611e743c2eaf08ffea7a7029
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-27 10:01:21 -0700

    fix: opt into person timeline top app bar

--- app/src/main/java/com/wasl/app/PersonTimelineScreen.kt
+++ app/src/main/java/com/wasl/app/PersonTimelineScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -38,6 +39,7 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun PersonTimelineScreen(
     state: PersonTimelineUiState,

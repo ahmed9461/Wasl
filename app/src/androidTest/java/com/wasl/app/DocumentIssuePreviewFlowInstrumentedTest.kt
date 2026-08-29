9064a92b67799640180d1cc2f5b4944606b52020
ahmed9461 <128654158+ahmed9461@users.noreply.github.com>
2026-08-29 13:39:27 -0700

    test(documents): fix Compose preview flow import

--- app/src/androidTest/java/com/wasl/app/DocumentIssuePreviewFlowInstrumentedTest.kt
+++ app/src/androidTest/java/com/wasl/app/DocumentIssuePreviewFlowInstrumentedTest.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollToNode
